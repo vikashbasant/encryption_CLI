578284c3701759afae4e0250d53dbff1ff710cdd
Vikash Basant <basantvikash360@gmail.com>
2023-01-23 11:19:30 +0530

    Creating Driver Class to generate random key

--- src/Symmetric.java
+++ src/Symmetric.java
@@ -1,9 +1,17 @@
+
+
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+import javax.xml.bind.DatatypeConverter;
 import java.security.SecureRandom;
+import java.util.logging.Logger;
+
+import static java.awt.SystemColor.info;
 
 /*----Class to create key(Symmetric)----*/
 public class Symmetric {
+
+    private static final Logger LOGGER = Logger.getLogger(String.valueOf(Symmetric.class));
 
     /*----AES Algorithm for Encryption----*/
     public static final String AES = "AES";
@@ -25,5 +33,17 @@ public class Symmetric {
 
         /*----Simply return that key----*/
         return key;
+    }
+
+    /*---- Driver Code----*/
+    public static void main(String[] args) throws Exception {
+        /*----This will create AES key----*/
+        SecretKey symmetricKey = createAESKey();
+
+        /*----Now encoded the symmetricKey with HexBinary format----*/
+        String encodedSymmetricKey = DatatypeConverter.printHexBinary(symmetricKey.getEncoded());
+        LOGGER.info("encodedSymmetricKey: " + encodedSymmetricKey);
+
+
     }
 }
